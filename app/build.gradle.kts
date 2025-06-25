050ca7d687ef61f3dfd3126a8039f881975e8783
harry-kuria <harrisonkuria254@gmail.com>
2025-06-25 12:39:28 +0300

    🌏🌏Release v1.0.2🌏🌏

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.harry.kharrency"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
