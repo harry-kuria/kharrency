ba9bc63facc5408d484029924243894acd6e7ada
harry-kuria <harrisonkuria254@gmail.com>
2025-06-25 16:12:45 +0300

    🌏🌏Release v1.0.3🌏🌏

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.harry.kharrency"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
